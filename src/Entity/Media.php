93e8eb165127b8153e4cdb6bc2b52f1a97e63186
Valérie Lavoisier <42351072+valavoisier@users.noreply.github.com>
2026-06-12 00:07:12 +0200

    fix(invites): exclure l’administratrice de la requête SQL et supprimer le chargement inutile (#10)
    * fix(invites): exclure l’administratrice de la requête SQL et supprimer le chargement inutile
    - Exclusion d’Ina (ROLE_ADMIN) directement dans la requête SQL de findActiveGuests()
    - Empêche Doctrine de charger ses médias et ses albums (fetch EAGER)
    - Supprime la requête SQL inutile déclenchée par le chargement d’Ina
    - La page Invités n’exécute désormais plus qu’une seule requête métier
    - La seconde requête visible dans le profiler provient du rechargement de l’utilisateur connecté par Symfony Security (comportement normal)
    - Déplacement de la pagination dans MediaRepository pour centraliser la logique d’accès aux données
    - Nettoyage du MediaController (suppression de la pagination côté contrôleur)
    
    * style: appliquer les corrections automatiques de php-cs-fixer

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -17,10 +17,10 @@ class Media
     /** @phpstan-ignore property.unusedType */
     private ?int $id = null;
 
-    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'medias', fetch: 'EAGER')]
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'medias')]
     private ?User $user = null;
 
-    #[ORM\ManyToOne(targetEntity: Album::class, fetch: 'EAGER', inversedBy: 'medias')]
+    #[ORM\ManyToOne(targetEntity: Album::class, inversedBy: 'medias')]
     private ?Album $album = null;
 
     #[ORM\Column]
